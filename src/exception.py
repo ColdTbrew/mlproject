36f06a8a1c1210bd078158af48a30b430045b2c1
ColdTbrew <shchoi8687@hotmail.com>
2025-02-05 19:44:29 +0900

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -6,7 +6,7 @@ def error_message_detail(error, error_detail:sys):
         exc_tb.tb_frame.f_code.co_filename, exc_tb.tb_lineno, str(error))
     return error_message
     
-class CustomError(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.message = error_message_detail(error_message, error_detail=error_detail)
